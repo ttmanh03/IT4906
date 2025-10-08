ba18431b36c940e9c53c634dd3c5fdb747c61e4f
donglam1824 <donglam0378378@gmail.com>
2025-10-08 18:39:22 +0700

    GA

--- download_data.py
+++ download_data.py
@@ -3,4 +3,4 @@ import kagglehub
 # Download latest version
 path = kagglehub.dataset_download("alexjhon82/under-water-sensor-dataset")
 
-print("Path to dataset files:", path)
+print("Path to dataset files:", path)#dataset này ko có tọa độ 3D của các nodes
